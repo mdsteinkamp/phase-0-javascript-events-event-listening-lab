b4e624acf136b9ff2780e47a187c71efd2b39f2d
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-01-01 10:27:17 -0800

    commit tree 16c05376be0022c0951dcfb8b7f2af531466c430

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+    function clickAlert() {
+        alert('I Was Clicked!');
+    }
+    input.addEventListener('click', clickAlert);
 }
+
